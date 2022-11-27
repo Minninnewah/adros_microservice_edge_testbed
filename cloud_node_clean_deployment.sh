896a84fb29ac4b7414e0a372775542aa2d05f9ec
Minninnewah <40040609+Minninnewah@users.noreply.github.com>
2022-11-27 23:29:23 +0100

    Create cloud_node_clean_deployment.sh

---
+++ cloud_node_clean_deployment.sh
@@ -0,0 +1,9 @@
+kubectl delete cloud_db_handler.yaml
+kubectl delete cloud_db.yaml
+kubectl delete front_end.yaml
+kubectl delete speed_analysis.yaml
+
+rm cloud_db_handler.yaml
+rm cloud_db.yaml
+rm front_end.yaml
+rm speed_analysis.yaml
