c2e3813746f30049559c0a5e1358d7572cb1e199
Minninnewah <40040609+Minninnewah@users.noreply.github.com>
2022-11-27 23:30:28 +0100

    Create edge_node_clean_deployment.sh

---
+++ edge_node_clean_deployment.sh
@@ -0,0 +1,9 @@
+kubectl delete edge_db.yaml 
+kubectl delete edge_db_handler.yaml
+kubectl delete speed_controller.yaml
+kubectl delete cars_management.yaml
+
+rm edge_db.yaml 
+rm edge_db_handler.yaml
+rm speed_controller.yaml
+rm cars_management.yaml
