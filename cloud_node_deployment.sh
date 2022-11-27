6ed9d7ced77cd16bf034043833881b1fceaa291d
Minninnewah <40040609+Minninnewah@users.noreply.github.com>
2022-11-27 23:31:21 +0100

    Update cloud_node_deployment.sh

--- cloud_node_deployment.sh
+++ cloud_node_deployment.sh
@@ -1,5 +1,5 @@
 # cloud_db_handler
-wget -O - https:/cloud_db_handler.yaml/raw.githubusercontent.com/Minninnewah/aucas_microservice_edge_testbed/main/services/cloud_db_handler/cloud_db_handler.yaml > cloud_db_handler.yaml
+wget -O - https://raw.githubusercontent.com/Minninnewah/aucas_microservice_edge_testbed/main/services/cloud_db_handler/cloud_db_handler.yaml > cloud_db_handler.yaml
 kubectl apply cloud_db_handler.yaml
 
 # cloud_db
