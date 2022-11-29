6b8cf1268af4d187a93a7bd14c22bb2a98ed4a07
heebzen1 <heebzen1@students.zhaw.ch>
2022-11-29 09:55:35 +0100

    Complete register and dregister function

--- services/cars_managment/index.js
+++ services/cars_managment/index.js
@@ -6,31 +6,32 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-let active_cars = {};
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+let active_cars = [];
 let nextCarIndex = 0;
 
 
-app.get('/register', async (req, res) => {
+app.get('/register/:carNr', async (req, res) => {
+  const carNr = req.params.carNr;
   console.log("register");
-  const id = req.body.id;
-  const ip = req.body.ip;
+  //const id = req.body.id;
 
 
-  active_cars[carNr] = req.ip + ":" + "3000"
-  //req.connection.remoteAddress
-  //req.connection.remotePort
-  //req.connection.localAddress
-  //req.connection.localPort
-  //const data = await got(db_handler_url).json();
+  active_cars.push({carNr: carNr, ip : "test"})
+  console.log(active_cars)
 
-  res.status(200).json(roadPieces);
+  res.status(200).send();
 });
 
-app.get('/deregister', async (req, res) => {
+app.get('/deregister/:carNr', async (req, res) => {
   console.log("deregister");
-  let carNR = req.query.id;
+  let carNR = req.params.carNr;
 
-  const index = active_cars.indexOf(carNR);
+  const index = active_cars.map( e => e.carNr).indexOf(carNR);
+  console.log(index);
+
   if (index > -1) {         // only splice array when item is found
     active_cars.splice(index, 1); // 2nd parameter means remove one item only
 
@@ -38,16 +39,18 @@ app.get('/deregister', async (req, res) => {
       nextCarIndex--;
     }
   }
-
-  res.status(200).json(roadPieces);
+  console.log("Active cars after deregister")
+  console.log(active_cars)
+  res.status(200).send();
 });
 
 
 const manage_cars = () => {
-  if (active_cars.length() > 0) {
-    got("speed-controller/" + active_cars[nextCarIndex]).json();
+  if (active_cars.length > 0) {
+    console.log(active_cars[nextCarIndex])
+    got("http://speed-controller:5000/" + active_cars[nextCarIndex].ip);
   }
-  if (active_cars.length() > nextCarIndex + 1) {
+  if (active_cars.length > nextCarIndex + 1) {
     nextCarIndex++;
   } else {
     nextCarIndex = 0;
